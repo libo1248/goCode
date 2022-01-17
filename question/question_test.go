448e5f2ebf41749bd44e82d0f71682b537ffd22e
LeeBo <libo1248@gmail.com>
2022-01-17 18:46:05 +0800

    update

--- question/question_test.go
+++ question/question_test.go
@@ -4,10 +4,18 @@ import (
 	"fmt"
 	"math/rand"
 	"testing"
+	"time"
 )
 
 func TestMaxNumLessThenN(t *testing.T) {
+	rand.Seed(time.Now().Unix())
 	n := uint32(rand.Int())
 	fmt.Println(n, getMaxNum(n))
+}
 
+func TestPermute(t *testing.T) {
+	nums := []uint32{1, 2, 3}
+	fmt.Println(nums)
+	fmt.Println("---------------")
+	fmt.Println(permute(nums))
 }
